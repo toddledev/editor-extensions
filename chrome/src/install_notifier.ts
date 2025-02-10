79a605ce499cee0791216cbb02bb968fcb930929
Marija Gerasimovska <marija@toddle.dev>
2025-02-10 13:27:41 +0100

    Run the content script when document start

--- chrome/src/install_notifier.ts
+++ chrome/src/install_notifier.ts
@@ -12,6 +12,23 @@ window.postMessage(
   '*',
 )
 
+document.onreadystatechange = (event) => {
+  if (document.readyState === 'complete') {
+    window.postMessage(
+      {
+        sender: 'toddle-extension',
+        message_name: 'info',
+        message: {
+          installed: true,
+          version: installedChromeVersion,
+        },
+        state: document.readyState,
+      },
+      '*',
+    )
+  }
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   window.postMessage(
     {
